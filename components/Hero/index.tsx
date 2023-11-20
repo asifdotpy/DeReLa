a2bfdb9f124ad01630c4d1572b4e9aa7f746f9e1
Asif Iqbal <asifdotpy@gmail.com>
2023-11-20 17:00:58 +0000

    Added content into Home

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -16,19 +16,16 @@ const Hero = () => {
           <div className="flex lg:items-center lg:gap-8 xl:gap-32.5">
             <div className=" md:w-1/2">
               <h4 className="mb-4.5 text-lg font-medium text-black dark:text-white">
-                🔥 Solid - A Complete SaaS Web Template
+                🔥 DeReLa - Decentralized Reinforcement Learning Agents
               </h4>
               <h1 className="mb-5 pr-16 text-3xl font-bold text-black dark:text-white xl:text-hero ">
-                Free Next.js Template for {"   "}
+                Worlds First Decentrasized {"   "}
                 <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg dark:before:bg-titlebgdark ">
-                  SaaS
+                  Agents
                 </span>
               </h1>
               <p>
-                Solid Pro - Packed with all the key integrations you need for
-                swift SaaS startup launch, including - Auth, Database, Sanity
-                Blog, Essential Components, Pages and More. Built-winth -
-                Next.js 13, React 18 and TypeScript.
+                “AI Revolution - Decentralized AI agents, an emerging concept, offer adaptability and autonomy surpassing traditional AI models. They operate independently or with others, perform tasks, test performance, and self-correct using reinforcement learning techniques. Mathematical models help illustrate these concepts and maximize rewards, optimizing system performance. This leads to the development of robust, resilient, and scalable AI systems capable of handling uncertainty and complexity.”
               </p>
 
               <div className="mt-10">
@@ -51,7 +48,7 @@ const Hero = () => {
                 </form>
 
                 <p className="mt-5 text-black dark:text-white">
-                  Try for free no credit card required.
+                  Sign up for our newsletter!
                 </p>
               </div>
             </div>
