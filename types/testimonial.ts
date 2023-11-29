22c470e1096cf2c7e494ba1ed2c65185cebaff81
Asif Iqbal <asifdotpy@gmail.com>
2023-11-29 16:58:05 +0000

    images issue fixed fortestimonial

--- types/testimonial.ts
+++ types/testimonial.ts
@@ -1,8 +1,10 @@
+import { StaticImageData } from 'next/image'
+
 export type Testimonial = {
-  id: number;
-  name: string;
-  designation: string;
-  image: string;
-  content: string;
-};
+    id: number,
+    name: string,
+    designation: string,
+    image: StaticImageData, // public/images/user images  
+    content: string,
+}
 
