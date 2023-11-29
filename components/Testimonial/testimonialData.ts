6084c5a3a5e444075e2da09ba7b048d0da3f5be2
Asif Iqbal <asifdotpy@gmail.com>
2023-11-29 16:20:07 +0000

    testimonial.ts file added in types directory to fix vercel build error.

--- components/Testimonial/testimonialData.ts
+++ components/Testimonial/testimonialData.ts
@@ -1,38 +1,38 @@
-import image1 from "@/public/images/user/user-01.png";
 import image2 from "@/public/images/user/user-02.png";
 import { Testimonial } from "@/types/testimonial";
 
 export const testimonialData: Testimonial[] = [
   {
     id: 1,
-    name: "Devid Smith",
-    designation: "Founter @democompany",
+    name: "Alice Lee",
+    designation: "CEO @smartcompany",
     image: image1,
     content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris hendrerit, ligula sit amet cursus tincidunt, lorem sem elementum nisi, convallis fringilla ante nibh non urna.",
+      "I was amazed by the performance of the decentralized reinforcement learning agents in my project. They were able to learn from each other and adapt to different environments without any central coordination. They also reduced the communication and computation costs significantly.",
   },
   {
     id: 2,
-    name: "Jhon Abraham",
-    designation: "Founter @democompany",
+    name: "Bob Chen",
+    designation: "CTO @innovativecompany",
     image: image2,
     content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris hendrerit, ligula sit amet cursus tincidunt, lorem sem elementum nisi, convallis fringilla ante nibh non urna.",
+      "The decentralized reinforcement learning agents were a game-changer for my project. They enabled me to scale up the learning process and handle complex tasks with multiple agents. They also improved the robustness and reliability of the system by avoiding single points of failure.",
   },
-  {
+ {
     id: 3,
-    name: "Devid Smith",
-    designation: "Founter @democompany",
+    name: "Alice Lee",
+    designation: "CEO @smartcompany",
     image: image1,
     content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris hendrerit, ligula sit amet cursus tincidunt, lorem sem elementum nisi, convallis fringilla ante nibh non urna.",
+      "The best part about the decentralized reinforcement learning agents was that they were easy to use and integrate with my existing code. I did not have to worry about the details of the algorithm or the network architecture. The agents were self-organizing and self-improving.",
   },
   {
     id: 4,
-    name: "Jhon Abraham",
-    designation: "Founter @democompany",
+    name: "Bob Chen",
+    designation: "CTO @innovativecompany",
     image: image2,
     content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris hendrerit, ligula sit amet cursus tincidunt, lorem sem elementum nisi, convallis fringilla ante nibh non urna.",
+      "I highly recommend the decentralized reinforcement learning agents for anyone who wants to solve challenging problems with multiple agents. They are the future of artificial intelligence and multi-agent systems. They are also fun to watch and interact with.",
   },
 ];
+
---
+++ types/testimonial.ts
@@ -0,0 +1,8 @@
+export type Testimonial = {
+  id: number;
+  name: string;
+  designation: string;
+  image: string;
+  content: string;
+};
+
